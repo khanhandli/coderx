1b188f1949b677069db237a61d34b42aa5cd96dc
Nguyennhuy <khanhandlinh12345@gmail.com>
2020-11-22 21:09:26 +0700

    bai tap npm

--- npm/index.js
+++ npm/index.js
@@ -6,10 +6,32 @@ var readlineSync = require('readline-sync');
 
 // console.log('Hello ' + userName + '!') ;
 // 
-var languages = [];
+// var languages = [];
 
-var language = readlineSync.question('Ngon ngu cua may la gi: ')
+// var language = readlineSync.question('Ngon ngu cua may la gi: ')
 
-languages.push(language);
+// languages.push(language);
 
-console.log(languages)
+// console.log(languages)
+
+do{
+	console.log('1. nhap name?');
+	console.log('2. hien thi?');
+	console.log('3. thoat?');
+	var chon = readlineSync.question('may chon so may?');
+	switch (parseInt(chon)) {
+		case 1:
+			var name = readlineSync.question('Ten may la gi? \n');
+			console.log('  ')
+			break;
+			case 2:
+			console.log(name);
+			break;
+			case 3:
+			console.log('Bye?');
+			break;
+		default:
+			console.log('Chon sai');
+			break;
+	}
+} while (chon < 3);
